67eb42c5a7abf15a6d6326788ffe4bd0c3372cb4
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2018-09-23 16:47:56 +0300

    add-login-form

--- src/components/Header/Login/LoginForm.jsx
+++ src/components/Header/Login/LoginForm.jsx
@@ -1,16 +1,37 @@
 import React from "react";
+import { API_URL, API_KEY_3 } from "../../../api/api";
 
 export default class LoginForm extends React.Component {
   state = {
     username: "",
     password: "",
-    errors: {}
+    errors: {},
+    submitting: false
   };
 
   onChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState(prevState => ({
+      [name]: value,
+      errors: {
+        ...prevState.errors,
+        [name]: null
+      }
+    }));
+  };
+
+  handleBlur = () => {
+    console.log("on blur");
+    const errors = this.validateFields();
+    if (Object.keys(errors).length > 0) {
+      this.setState(prevState => ({
+        errors: {
+          ...prevState.errors,
+          ...errors
+        }
+      }));
+    }
   };
 
   validateFields = () => {
@@ -23,13 +44,97 @@ export default class LoginForm extends React.Component {
     return errors;
   };
 
+  onSubmit = () => {
+    const fetchApi = (url, options = {}) => {
+      return new Promise((resolve, reject) => {
+        fetch(url, options)
+          .then(response => {
+            if (response.status < 400) {
+              return response.json();
+            } else {
+              throw response;
+            }
+          })
+          .then(data => {
+            resolve(data);
+          })
+          .catch(response => {
+            response.json().then(error => {
+              reject(error);
+            });
+          });
+      });
+    };
+    this.setState({
+      submitting: true
+    });
+    fetchApi(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
+      .then(data => {
+        return fetchApi(
+          `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`,
+          {
+            method: "POST",
+            mode: "cors",
+            headers: {
+              "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+              username: this.state.username,
+              password: this.state.password,
+              request_token: data.request_token
+            })
+          }
+        );
+      })
+      .then(data => {
+        return fetchApi(
+          `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`,
+          {
+            method: "POST",
+            mode: "cors",
+            headers: {
+              "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+              request_token: data.request_token
+            })
+          }
+        );
+      })
+      .then(data => {
+        console.log("session", data);
+        this.setState({
+          submitting: false
+        });
+      })
+      .catch(error => {
+        console.log("error", error);
+        this.setState({
+          submitting: false,
+          errors: {
+            base: error.status_message
+          }
+        });
+      });
+  };
+
   onLogin = e => {
     e.preventDefault();
     const errors = this.validateFields();
+    if (Object.keys(errors).length > 0) {
+      this.setState(prevState => ({
+        errors: {
+          ...prevState.errors,
+          ...errors
+        }
+      }));
+    } else {
+      this.onSubmit();
+    }
   };
 
   render() {
-    const { username, password, errors } = this.state;
+    const { username, password, errors, submitting } = this.state;
     return (
       <div className="form-login-container">
         <form className="form-login">
@@ -46,6 +151,7 @@ export default class LoginForm extends React.Component {
               name="username"
               value={username}
               onChange={this.onChange}
+              onBlur={this.handleBlur}
             />
             {errors.username && (
               <div className="invalid-feedback">{errors.username}</div>
@@ -70,9 +176,13 @@ export default class LoginForm extends React.Component {
             type="submit"
             className="btn btn-lg btn-primary btn-block"
             onClick={this.onLogin}
+            disabled={submitting}
           >
             Вход
           </button>
+          {errors.base && (
+            <div className="invalid-feedback text-center">{errors.base}</div>
+          )}
         </form>
       </div>
     );
