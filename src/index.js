d76ed67ab24c5da99c3ff5573c3a7634ce7008ae
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2018-10-11 22:08:39 +0300

    update

--- src/index.js
+++ src/index.js
@@ -3,5 +3,47 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./stylesheets/index.css";
+import { createStore } from "redux";
 
+const actionCreatorUpdateAuth = payload => {
+  return {
+    type: "UPDATE_AUTH",
+    payload
+  };
+};
+
+const initialState = {
+  user: null,
+  session_id: null,
+  isAuth: false
+};
+
+const reducerApp = (state = initialState, action) => {
+  console.log("reducerApp", state, action);
+  switch (action.type) {
+    case "UPDATE_AUTH":
+      return {
+        ...state,
+        user: action.payload.user,
+        session_id: action.payload.session_id,
+        isAuth: true
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducerApp);
+
+console.log("store", store);
+console.log("getState", store.getState());
+store.dispatch(
+  actionCreatorUpdateAuth({
+    user: {
+      name: "Evgeniy"
+    },
+    session_id: "text"
+  })
+);
+console.log("after update Auth", store.getState());
 ReactDOM.render(<App />, document.getElementById("root"));
