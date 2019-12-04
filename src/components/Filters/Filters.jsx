9504da120f5240384923d939df5031dd89437ebf
Loriella <loris.orlova@gmail.com>
2019-12-04 18:02:30 +0300

    select placed in a separate component

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SortBy from './SortBy';
 
 export default class Filters extends React.Component {
   render() {
@@ -10,21 +11,10 @@ export default class Filters extends React.Component {
     } = this.props;
     return (
       <form className="mb-3">
-        <div className="form-group">
-          <label htmlFor="sort_by">Сортировать по:</label>
-          <select
-            id="sort_by"
-            name="sort_by"
-            className="form-control"
-            value={sort_by}
-            onChange={onChangeFilters}
-          >
-            <option value="popularity.desc">Популярные по убыванию</option>
-            <option value="popularity.asc">Популярные по возростанию</option>
-            <option value="vote_average.desc">Рейтинг по убыванию</option>
-            <option value="vote_average.asc">Рейтинг по возростанию</option>
-          </select>
-        </div>
+        <SortBy
+          sort_by={sort_by}
+          onChangeFilters={onChangeFilters}
+        />
         <div className="btn-group">
           <button
             type="button"
