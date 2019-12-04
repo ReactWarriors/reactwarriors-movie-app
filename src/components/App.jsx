4c5b9a6cf19a3b5ad0390cafd351e9e3fdb32a8e
Loriella <loris.orlova@gmail.com>
2019-12-04 17:22:05 +0300

    add logic App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,20 +3,60 @@ import Filters from "./Filters/Filters";
 import MoviesList from "./Movies/MoviesList";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filters: {
+        sort_by: "popularity.desc"
+      },
+      page: 1
+    }
+  }
+
+  onChangeFilters = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState(prevState => ({
+      filters: {
+        ...prevState.filters,
+        [name]: value
+      }
+    }));
+  };
+
+  onChangePage = page => {
+    this.setState({
+      page
+    })
+  };
+
   render() {
+    const {filters, page} = this.state;
+
     return (
       <div className="container">
         <div className="row mt-4">
           <div className="col-4">
-            <div className="card" style={{ width: "100%" }}>
+            <div className="card" style={{width: "100%"}}>
               <div className="card-body">
                 <h3>Фильтры:</h3>
-                <Filters />
+                <Filters
+                  filters={filters}
+                  page={page}
+                  onChangeFilters={this.onChangeFilters}
+                  onChangePage={this.onChangePage}
+                />
               </div>
             </div>
           </div>
           <div className="col-8">
-            <MoviesList />
+            <MoviesList
+              filters={filters}
+              page={page}
+              onChangePage={this.onChangePage}
+            />
           </div>
         </div>
       </div>
