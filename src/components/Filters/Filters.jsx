e4da331163da5888b3cf4513bd0d3f25a4484a62
Loriella <loris.orlova@gmail.com>
2019-12-04 17:21:48 +0300

    add logic Filters

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -2,16 +2,45 @@ import React from "react";
 
 export default class Filters extends React.Component {
   render() {
+    const {
+      filters: {sort_by},
+      onChangeFilters,
+      page,
+      onChangePage
+    } = this.props;
     return (
       <form className="mb-3">
         <div className="form-group">
           <label htmlFor="sort_by">Сортировать по:</label>
-          <select className="form-control" id="sort_by">
+          <select
+            id="sort_by"
+            name="sort_by"
+            className="form-control"
+            value={sort_by}
+            onChange={onChangeFilters}
+          >
             <option value="popularity.desc">Популярные по убыванию</option>
             <option value="popularity.asc">Популярные по возростанию</option>
             <option value="vote_average.desc">Рейтинг по убыванию</option>
             <option value="vote_average.asc">Рейтинг по возростанию</option>
           </select>
+        </div>
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn btn-light"
+            disabled={page === 1}
+            onClick={onChangePage.bind(null, page - 1)}
+          >
+            Назад
+          </button>
+          <button
+            type="button"
+            className="btn btn-light"
+            onClick={onChangePage.bind(null, page + 1)}
+          >
+            Вперед
+          </button>
         </div>
       </form>
     );
