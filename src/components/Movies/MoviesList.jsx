dd2d7f24afa5aa6b57c6bcf2f1b831170020fe00
Loriella <loris.orlova@gmail.com>
2019-12-08 17:56:30 +0300

    add navigation

--- src/components/Movies/MoviesList.jsx
+++ src/components/Movies/MoviesList.jsx
@@ -1,73 +1,40 @@
 import React, {Component} from "react";
 import MovieItem from "./MovieItem";
 import {API_URL, API_KEY_3} from "../../api/api";
+import Pagination from "./Pagination";
 
 export default class MovieList extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      movies: []
+      movies: [],
+      total_pages: 1,
     };
   }
 
   getMovies = (filters, page) => {
-    const { sort_by } = filters;
+    const {sort_by, primary_release_year, with_genres} = filters;
 
-    const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&language=ru-RU&sort_by=${sort_by}&page=${page}`;
+    const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&language=ru-RU&sort_by=${sort_by}&page=${page}&primary_release_year=${primary_release_year}&with_genres=${with_genres}`;
     fetch(link)
       .then(response => {
         return response.json();
       })
       .then(data => {
         this.setState({
-          movies: data.results
+          movies: data.results,
+          total_pages: data.total_pages,
         });
       });
   };
 
   componentDidMount() {
-    // const sort_by = this.props.filters.sort_by;
-    // const {
-    //   filters: {sort_by}
-    // } = this.props;
-    //
-    // const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&language=ru-RU&sort_by=${sort_by}`;
-    // fetch(link)
-    //   .then(response => {
-    //     return response.json();
-    //   })
-    //   .then(data => {
-    //     this.setState({
-    //       movies: data.results
-    //     });
-    //   });
-    this.getMovies(this.props.filters, this.props.page);
+    this.getMovies(this.props.filters, this.props.primary_release_year, this.props.page);
   }
 
-  //Устаревший способ
-  // componentWillReceiveProps(nextProps) {
-  //   if (nextProps.filters.sort_by !== this.props.filters.sort_by) {
-      // const {
-      //   filters: {sort_by}
-      // } = this.props;
-      //
-      // const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&language=ru-RU&sort_by=${sort_by}`;
-      // fetch(link)
-      //   .then(response => {
-      //     return response.json();
-      //   })
-      //   .then(data => {
-      //     this.setState({
-      //       movies: data.results
-      //     });
-      //   });
-      // this.getMovies(nextProps.filters);
-  //   }
-  // }
-
-  componentDidUpdate(prevProps){
-    if (this.props.filters.sort_by !== prevProps.filters.sort_by){
+  componentDidUpdate(prevProps) {
+    if (this.props.filters !== prevProps.filters) {
       this.props.onChangePage(1);
       this.getMovies(this.props.filters, 1);
     }
@@ -78,7 +45,9 @@ export default class MovieList extends Component {
   }
 
   render() {
-    const {movies} = this.state;
+    const {movies, total_pages} = this.state;
+    const {page, onChangePage} = this.props;
+
     return (
       <div className="row">
         {movies.map(movie => {
@@ -88,6 +57,15 @@ export default class MovieList extends Component {
             </div>
           );
         })}
+        {!movies.length && <div className="mx-auto mt-4">Ничего не найдено</div>}
+        {
+          !!movies.length &&
+          <Pagination
+            page={page}
+            total_pages={total_pages}
+            onChangePage={onChangePage}
+          />
+        }
       </div>
     );
   }
