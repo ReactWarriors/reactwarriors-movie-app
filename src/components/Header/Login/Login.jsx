2eeb1e4f2e4deb0da0cb67e04b0eff6d9ea5578a
Loriella <loris.orlova@gmail.com>
2019-12-13 21:10:52 +0300

    add Login

---
+++ src/components/Header/Login/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <button
+          className="btn btn-success"
+          type="button"
+        >
+          Login
+        </button>
+      </div>
+    );
+  }
+}
