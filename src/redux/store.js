f27ca2a09375b0147ab9897648e78f92e1a0764d
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2019-12-22 19:34:11 +0200

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,17 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+const logger = store => next => action => {
+  console.log("store", store.getState());
+  console.log("type", action.type);
+  console.log("payload", action.payload);
+  return next(action);
+};
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
