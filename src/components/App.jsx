d7b374c14e62a55b8be2713c92d0ec99f4922f71
Loriella <loris.orlova@gmail.com>
2020-01-14 15:08:18 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import Filters from "./Filters/Filters";
-import MoviesList from "./Movies/MoviesList";
 import Header from "./Header/Header";
 import LoginModal from "./Modals/LoginModal";
+import MoviesPage from "./pages/MoviesPage/MoviesPage";
 import CallApi from "../api/api";
 import _ from "lodash";
 import Cookies from "universal-cookie";
@@ -10,13 +9,6 @@ import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
 export const AppContext = React.createContext();
-
-
-const initialFilters = {
-  sort_by: "popularity.desc",
-  primary_release_year: "primary_release_year",
-  with_genres: [],
-};
 
 export default class App extends React.Component {
   constructor(props) {
@@ -27,9 +19,6 @@ export default class App extends React.Component {
       favorites: [],
       watchlist: [],
       session_id: null,
-      filters: initialFilters,
-      page: 1,
-      total_pages: 1,
       showLoginModal: false,
     }
   }
@@ -91,37 +80,6 @@ export default class App extends React.Component {
     })
   };
 
-  onChangeFilters = event => {
-    const value = event.target.value;
-    const name = event.target.name;
-
-    this.setState(prevState => ({
-      filters: {
-        ...prevState.filters,
-        [name]: value
-      }
-    }));
-  };
-
-  onChangePage = page => {
-    this.setState({
-      page
-    })
-  };
-
-  onChangeTotalPages = value => {
-    this.setState({
-      total_pages: value
-    })
-  };
-
-  onResetFilters = () => {
-    this.setState({
-      filters: initialFilters,
-      page: 1
-    })
-  };
-
   getWatchlist = () => {
     const {session_id, user} = this.state;
 
@@ -154,10 +112,6 @@ export default class App extends React.Component {
 
   render() {
     const {
-      filters,
-      page,
-      total_pages,
-      genres,
       user,
       session_id,
       favorites,
@@ -186,36 +140,7 @@ export default class App extends React.Component {
             user={user}
             updateSessionId={this.updateSessionId}
           />
-          <div className="container pt-1">
-            <div className="row">
-              <div className="col-4">
-                <div className="card sticky-top">
-                  <div className="card-body py-1">
-                    <h3>Фильтры:</h3>
-                    <Filters
-                      filters={filters}
-                      page={page}
-                      total_pages={total_pages}
-                      genres={genres}
-                      onChangeFilters={this.onChangeFilters}
-                      onChangePage={this.onChangePage}
-                      onResetFilters={this.onResetFilters}
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="col-8">
-                <MoviesList
-                  filters={filters}
-                  page={page}
-                  session_id={session_id}
-                  onChangePage={this.onChangePage}
-                  onChangeTotalPages={this.onChangeTotalPages}
-                  toggleModal={this.toggleModal}
-                />
-              </div>
-            </div>
-          </div>
+          <MoviesPage/>
           {
             showLoginModal && <LoginModal/>
           }
---
+++ src/components/pages/MoviesPage/MoviesPage.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import AppContextHOC from "../../HOC/AppContextHOC";
+import Filters from "../../Filters/Filters";
+import MoviesList from "../../Movies/MoviesList";
+
+const initialFilters = {
+  sort_by: "popularity.desc",
+  primary_release_year: "primary_release_year",
+  with_genres: [],
+};
+
+class MoviesPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filters: initialFilters,
+      page: 1,
+      total_pages: 1,
+    }
+  }
+
+  onChangeFilters = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState(prevState => ({
+      filters: {
+        ...prevState.filters,
+        [name]: value
+      }
+    }));
+  };
+
+  onChangePage = page => {
+    this.setState({
+      page
+    })
+  };
+
+  onChangeTotalPages = value => {
+    this.setState({
+      total_pages: value
+    })
+  };
+
+  onResetFilters = () => {
+    this.setState({
+      filters: initialFilters,
+      page: 1
+    })
+  };
+
+  render() {
+    const {
+      filters,
+      page,
+      total_pages,
+    } = this.state;
+
+    const {
+      session_id,
+      toggleModal
+    } = this.props;
+
+    return (
+      <div className="container pt-1">
+        <div className="row">
+          <div className="col-4">
+            <div className="card sticky-top">
+              <div className="card-body py-1">
+                <h3>Фильтры:</h3>
+                <Filters
+                  filters={filters}
+                  page={page}
+                  total_pages={total_pages}
+                  onChangeFilters={this.onChangeFilters}
+                  onChangePage={this.onChangePage}
+                  onResetFilters={this.onResetFilters}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col-8">
+            <MoviesList
+              filters={filters}
+              page={page}
+              session_id={session_id}
+              onChangePage={this.onChangePage}
+              onChangeTotalPages={this.onChangeTotalPages}
+              toggleModal={toggleModal}
+            />
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default AppContextHOC(MoviesPage);
