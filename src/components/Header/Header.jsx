d32aa26ee119028e3f1f63974e6dbb7645bc3970
Loriella <loris.orlova@gmail.com>
2020-01-21 15:18:03 +0300

    add Route

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 import Login from "./Login/Login";
 import UserMenu from "./Login/UserMenu";
+import {Route, Link} from "react-router-dom";
 
 class Header extends React.Component {
 
   render() {
     const {user} = this.props;
-    return (
-      <nav className="navbar navbar-dark bg-primary">
-        <div className="container">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">Home</a>
-            </li>
-          </ul>
-          {
-            user
-              ? <UserMenu/>
-              : <Login/>
-          }
 
-        </div>
-      </nav>
+    return (
+      <Route>
+        <nav className="navbar navbar-dark bg-primary">
+          <div className="container">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+            </ul>
+            {
+              user
+                ? <UserMenu/>
+                : <Login/>
+            }
+          </div>
+        </nav>
+      </Route>
     );
   }
 }
